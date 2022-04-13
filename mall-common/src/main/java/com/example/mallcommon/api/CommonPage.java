9596e0ff027f7f962e398a183673c4a5ca738b74
issueszz <issueszz@outlook.com>
2022-04-13 14:42:13 +0800

    清洗SpringData分页信息

--- mall-common/src/main/java/com/example/mallcommon/api/CommonPage.java
+++ mall-common/src/main/java/com/example/mallcommon/api/CommonPage.java
@@ -1,18 +1,21 @@
 package com.example.mallcommon.api;
 
 import com.github.pagehelper.PageInfo;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
-@Data
+@Setter
+@Getter
 public class CommonPage<T> {
     private Integer pageNum;
     private Integer pageSize;
     private Integer totalPage;
     private Long total;
     private List<T> list;
-
+    /*将PageHelper 分页后的list转为分页信息*/
     public static <T> CommonPage<T> restPage(List<T> list) {
         CommonPage<T> result = new CommonPage<T>();
         PageInfo<T> pageInfo = new PageInfo<T>(list);
@@ -21,6 +24,16 @@ public class CommonPage<T> {
         result.setTotalPage(pageInfo.getPages());
         result.setTotal(pageInfo.getTotal());
         result.setList(pageInfo.getList());
+        return result;
+    }
+    /*转化SpringData分页信息*/
+    public static <T> CommonPage<T> restPage(Page<T> pageInfo) {
+        CommonPage<T> result = new CommonPage<T>();
+        result.setTotalPage(pageInfo.getTotalPages());
+        result.setPageNum(pageInfo.getNumber());
+        result.setPageSize(pageInfo.getSize());
+        result.setTotal(pageInfo.getTotalElements());
+        result.setList(pageInfo.getContent());
         return result;
     }
 }
